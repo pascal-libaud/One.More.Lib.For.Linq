39ef1c9b4ab24c72ea0761b48a136fee54c349e0
pascal-libaud <pascal.libaud@outlook.fr>
2024-02-15 15:00:02 +0100

    New OmHasDuplicate method and its tests

---
+++ src/One.More.Lib.For.Linq/Helper/OmHasDuplicate.cs
@@ -0,0 +1,28 @@
+﻿namespace One.More.Lib.For.Linq.Helper;
+
+public static partial class LinqHelper
+{
+    public static bool OmHasDuplicate<T>(this IEnumerable<T> source)
+    {
+        var hash = new HashSet<T>();
+        foreach (var item in source)
+        {
+            if (!hash.Add(item))
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool OmHasDuplicate<T, U>(this IEnumerable<T> source, Func<T, U> selector)
+    {
+        var hash = new HashSet<U>();
+        foreach (var item in source)
+        {
+            if (!hash.Add(selector(item)))
+                return true;
+        }
+
+        return false;
+    }
+}
