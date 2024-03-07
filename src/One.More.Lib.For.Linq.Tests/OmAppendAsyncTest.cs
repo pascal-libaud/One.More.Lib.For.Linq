a71cfdf65a832a38571160df00bccc6b48f6f176
Pascal L <102244862+pascal-libaud@users.noreply.github.com>
2024-03-07 12:06:33 +0100

    New tests on OmAppendAsync and OmPrepredAsync methods. (#25)

---
+++ src/One.More.Lib.For.Linq.Tests/OmAppendAsyncTest.cs
@@ -0,0 +1,29 @@
+﻿namespace One.More.Lib.For.Linq.Tests;
+
+public class OmAppendAsyncTest : TestBase
+{
+    [Fact]
+    public async Task OmAppendAsync_should_add_in_end_when_source_not_null()
+    {
+        static async IAsyncEnumerable<string> GetValuesAsync()
+        {
+            await Task.Yield();
+            yield return "P";
+            yield return "Q";
+        }
+
+        Assert.Equal(new[] { "P", "Q", "R" }, await GetValuesAsync().OmAppendAsync("R").OmToListAsync());
+    }
+
+    [Fact]
+    public async Task OmAppendAsync_should_work_well_when_source_empty()
+    {
+        static async IAsyncEnumerable<string> GetValuesAsync()
+        {
+            await Task.Yield();
+            yield break;
+        }
+
+        Assert.Equal(new[] { "R" }, await GetValuesAsync().OmAppendAsync("R").OmToListAsync());
+    }
+}
