51c99a8488e2512f260ba5869564fd35bbd1a21a
pascal-libaud <pascal.libaud@outlook.fr>
2024-03-15 07:11:26 +0100

    Add OmToArrayAsync method.

---
+++ src/One.More.Lib.For.Linq.Async/OmToArrayAsync.cs
@@ -0,0 +1,9 @@
+﻿namespace One.More.Lib.For.Linq.Async;
+
+public static partial class LinqAsyncHelper
+{
+    public static async Task<T[]> OmToArrayAsync<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default)
+    {
+        return (await source.OmToListAsync(cancellationToken)).ToArray();
+    }
+}
