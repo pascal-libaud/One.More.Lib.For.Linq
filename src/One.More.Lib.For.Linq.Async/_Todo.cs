9631dc907b774b417ab140dda0175c3cd1a2ba36
pascal-libaud <pascal.libaud@outlook.fr>
2024-03-18 21:43:00 +0100

    Add OmFirst, OmFirstOrDefault, OmSingle and OmSingleOrDefault

--- src/One.More.Lib.For.Linq.Async/_Todo.cs
+++ src/One.More.Lib.For.Linq.Async/_Todo.cs
@@ -3,13 +3,10 @@
 // OmAggregateAsync
 // CastAsync
 // CrossJoinAsync
-// FirstAsync, FirstOrDefaultAsync
 // GroupByAsync
 // HasDuplicateAsync
 // OrderByAsync, ThenByAsync
 // ReverseAsync
-// SingleAsync
-// SingleOrDefaultAsync
 // SkipAsync, SkipLastAsync
 // ZipAsync
 // RepeatAsync
