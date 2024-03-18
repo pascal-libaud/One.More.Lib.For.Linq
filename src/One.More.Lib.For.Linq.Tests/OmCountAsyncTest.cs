e616d07f850c42c5c9433d15dcb8f052402c701c
pascal-libaud <pascal.libaud@outlook.fr>
2024-03-18 15:12:03 +0100

    Add CancellationToken support on OmCount

--- src/One.More.Lib.For.Linq.Tests/OmCountAsyncTest.cs
+++ src/One.More.Lib.For.Linq.Tests/OmCountAsyncTest.cs
@@ -5,7 +5,7 @@ public class OmCountAsyncTest : TestBase
     [Fact]
     public async Task OmCountAsync_should_work_as_expected()
     {
-        Assert.Equal(10, await SpyAsyncEnumerable.GetValuesAsync().OmCountAsync());
+        Assert.Equal(10, await LinqAsyncHelper.RangeAsync(10).OmCountAsync());
     }
 
     [Fact]
@@ -15,5 +15,30 @@ public class OmCountAsyncTest : TestBase
 
         _ = await spy.OmCountAsync();
         Assert.Equal(1, spy.CountEnumeration);
+    }
+
+    [Fact]
+    public async Task OmDistinctAsync_should_pass_cancellation_token()
+    {
+        var token = new CancellationTokenSource();
+        await token.CancelAsync();
+
+        var func = async () => await LinqAsyncHelper.RangeAsync(10).OmCountAsync(token.Token);
+        await func.Should().ThrowAsync<OperationCanceledException>();
+    }
+
+    [Fact]
+    public async Task OmDistinctAsync_should_pass_cancellation_token_bis()
+    { 
+        IAsyncEnumerable<int> GetValues()
+        {
+            return LinqAsyncHelper.RangeAsync(10);
+        }
+
+        var token = new CancellationTokenSource();
+        await token.CancelAsync();
+
+        var func = async () => await GetValues().OmCountAsync(token.Token);
+        await func.Should().ThrowAsync<OperationCanceledException>();
     }
 }
