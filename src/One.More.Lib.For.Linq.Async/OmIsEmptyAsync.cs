2e574d6bc85fb1a84a460e0b9b0bccb83f781e9b
pascal-libaud <pascal.libaud@outlook.fr>
2024-03-21 09:11:07 +0100

    OmIsEmpty and OmIsEmptyAsync methods

---
+++ src/One.More.Lib.For.Linq.Async/OmIsEmptyAsync.cs
@@ -0,0 +1,12 @@
+﻿namespace One.More.Lib.For.Linq.Async;
+
+public static partial class LinqAsyncHelper
+{
+    public static async Task<bool> OmIsEmptyAsync<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default)
+    {
+        await foreach (var _ in source.WithCancellation(cancellationToken))
+            return false;
+
+        return true;
+    }
+}
