c6c08d4c1fdedc73c503022be6e13061969d67af
pascal-libaud <pascal.libaud@outlook.fr>
2024-04-03 14:00:23 +0200

    Add support of IWithCount on Empty<T>

--- src/One.More.Lib.For.Linq/Empty.cs
+++ src/One.More.Lib.For.Linq/Empty.cs
@@ -4,6 +4,11 @@ public static partial class LinqHelper
 {
     public static IEnumerable<T> Empty<T>()
     {
+        return new EnumerableWithCount<T>(InnerEmpty<T>(), () => 0);
+    }
+
+    private static IEnumerable<T> InnerEmpty<T>()
+    {
         yield break;
     }
 }
