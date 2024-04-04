6cab96c01df50b797844cad101ff7395bcae2b8c
pascal-libaud <pascal.libaud@outlook.fr>
2024-04-04 17:58:20 +0200

    Add support of IWithCount on Repeat method.

--- src/One.More.Lib.For.Linq/Repeat.cs
+++ src/One.More.Lib.For.Linq/Repeat.cs
@@ -4,6 +4,11 @@ public static partial class LinqHelper
 {
     public static IEnumerable<T> Repeat<T>(T item, int count)
     {
+        return new EnumerableWithCount<T>(InnerRepeat(item, count), () => count);
+    }
+
+    private static IEnumerable<T> InnerRepeat<T>(T item, int count)
+    {
         for (int i = 0; i < count; i++)
             yield return item;
     }
