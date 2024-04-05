05fa5e53712d9d410af26b49a5ec592c1a04e9ab
pascal-libaud <pascal.libaud@outlook.fr>
2024-04-05 07:16:30 +0200

    Add support of IWithCount on Range

--- src/One.More.Lib.For.Linq/Range.cs
+++ src/One.More.Lib.For.Linq/Range.cs
@@ -1,27 +1,37 @@
-﻿namespace One.More.Lib.For.Linq;
+﻿using System.Reflection.Metadata;
+
+namespace One.More.Lib.For.Linq;
 
 public static partial class LinqHelper
 {
     public static IEnumerable<T> Range<T>(T count) where T : INumber<T>
     {
-        for (T i = T.Zero; i < count; i++)
-            yield return i;
+        return new EnumerableWithCount<T>(InnerRange(T.Zero, count), () => int.CreateChecked(count));
     }
 
     public static IEnumerable<T> Range<T>(T start, T count) where T : INumber<T>
+    {
+        return new EnumerableWithCount<T>(InnerRange(start, count), () => int.CreateChecked(count));
+    }
+
+    public static IEnumerable<T?> RangeNullable<T>(T count) where T : struct, INumber<T>
+    {
+        return new EnumerableWithCount<T?>(InnerRangeNullable(T.Zero, count), () => int.CreateChecked(count));
+    }
+
+    public static IEnumerable<T?> RangeNullable<T>(T start, T count) where T : struct, INumber<T>
+    {
+        return new EnumerableWithCount<T?>(InnerRangeNullable(start, count), () => int.CreateChecked(count));
+    }
+
+    private static IEnumerable<T> InnerRange<T>(T start, T count) where T : INumber<T>
     {
         var end = start + count;
         for (T i = start; i < end; i++)
             yield return i;
     }
 
-    public static IEnumerable<T?> RangeNullable<T>(T count) where T : INumber<T>
-    {
-        for (T i = T.Zero; i < count; i++)
-            yield return i;
-    }
-
-    public static IEnumerable<T?> RangeNullable<T>(T start, T count) where T : struct, INumber<T>
+    private static IEnumerable<T?> InnerRangeNullable<T>(T start, T count) where T : struct, INumber<T>
     {
         var end = start + count;
         for (T i = start; i < end; i++)
